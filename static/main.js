ada41199d4a750ed11f68fbc4c61533b1b5d31cf
SunnyPuss <fomhumster@ya.ru>
2019-06-22 01:10:28 +0300

    constructor question

---
+++ static/main.js
@@ -0,0 +1,8 @@
+class Profile {
+    constructor (userName, realName, password) {
+        this.name = new String (userName);
+        this.realName = realName;
+        this.password = new String (password);
+        realName = {name, surname};
+    }
+}
